74d1ca768f3fdb80c016f21f0a6ff55366270feb
gdtoprak <gd.toprak@gmail.com>
2016-05-17 16:26:44 -0500

    Alert but too many

--- TaskMapper/src/components/map/UltimateMap.js
+++ TaskMapper/src/components/map/UltimateMap.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import {
   MapView,
   AsyncStorage,
+  AlertIOS,
 } from 'react-native'
 
 var chicagoRegion = {
@@ -52,10 +53,12 @@ function makeMarker(task) {
   var fracThrough = (currentTime-setTime)/(dueTime-setTime)
 
   return {
+    name: task.name,
     latitude: task.location.lat,
     longitude: task.location.lng,
     radius: impendingRadius(fracThrough),
     color: impendingColor(fracThrough),
+    open: true,
   }
 }
 
@@ -91,34 +94,73 @@ function hslToRgb(h, s, l){
     return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
 
+function inCircle(self, marker) {
+  var tau = 2 * Math.PI
+  var avgY = (self.latitude + marker.latitude)/2
+
+  var yDist = (self.latitude - marker.latitude)*111111.1
+  var xDist = (self.longitude - marker.longitude)*(111111.1 * Math.cos(avgY*tau/360))
+
+  var dist = Math.sqrt( Math.pow(xDist, 2) + Math.pow(yDist, 2) )
+  return (dist < marker.radius)
+}
+
 var UltimateMap = React.createClass({
-  // componentDidMount: function() {
-  //   AsyncStorage.getItem("tasks")
-  //   .then( (rawTasks) => JSON.parse(rawTasks))
-  //   .then( (tasks) => {
-  //     this.setState({markers: tasks.map(makeMarker)})
-  //   })
-  // },
-  // componentWillReceiveProps: function() {
-  //   AsyncStorage.getItem("tasks")
-  //   .then( (rawTasks) => JSON.parse(rawTasks))
-  //   .then( (tasks) => {
-  //     this.setState({markers: tasks.map(makeMarker)})
-  //   })
-  // },
   getInitialState: function() {
     return {
       region: chicagoRegion,
       markers: [],
+      myPosition: {
+        latitude: 0,
+        longitude: 0,
+      },
     }
   },
-  render: function() {
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var myPosition = {
+          latitude: parseFloat(position.coords.latitude),
+          longitude: parseFloat(position.coords.longitude),
+        }
+        this.setState({
+          myPosition: myPosition
+        })
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 5000}
+    )
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = {
+        lat: parseFloat(position.coords.latitude),
+        long: parseFloat(position.coords.longitude),
+      }
+      this.setState({
+        lastPosition
+      })
+    })
+  },
+  componentWillReceiveProps: function() {
     // AsyncStorage.clear()
     AsyncStorage.getItem("tasks")
     .then( (rawTasks) => JSON.parse(rawTasks))
     .then( (tasks) => {
       this.setState({markers: tasks.map(makeMarker)})
     })
+
+    var self = this.state.myPosition
+    var newMarkers = []
+    for (var i in this.state.markers) {
+      var marker = this.state.markers[i]
+      if (marker.open && inCircle(self, marker)) {
+        AlertIOS.alert(marker.name)
+        marker.open = false
+      }
+      newMarkers.push(marker)
+    }
+    this.setState({markers: newMarkers})
+  },
+  render: function() {
     return (
       <MapView style={{flex: 1}}
       region={this.state.region}
@@ -128,33 +170,9 @@ var UltimateMap = React.createClass({
       />
     )
   },
-  // componentDidMount() {
-  //   navigator.geolocation.getCurrentPosition(
-  //     (position) => {
-  //       var initialPosition = {
-  //         long: parseFloat(position.coords.longitude),
-  //         lat: parseFloat(position.coords.latitude)
-  //       }
-  //       this.setState({
-  //         initialPosition
-  //       })
-  //     },
-  //     (error) => alert(error.message),
-  //     {enableHighAccuracy: true, timeout: 20000, maximumAge: 5000}
-  //   )
-  //   this.watchID = navigator.geolocation.watchPosition((position) => {
-  //     var lastPosition = {
-  //       long: parseFloat(position.coords.longitude),
-  //       lat: parseFloat(position.coords.latitude)
-  //     }
-  //     this.setState({
-  //       lastPosition
-  //     })
-  //   })
-  // },
-  // componentWillUnmount() {
-  //   navigator.geolocation.clearWatch(this.watchID)
-  // },
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID)
+  },
 })
 
 module.exports = UltimateMap
